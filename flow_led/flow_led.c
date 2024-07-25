d16c633650493cc3a7b127cca0ac906f1031ec8d
Jyri Kajava <73351309+kajava-juri@users.noreply.github.com>
2024-07-25 13:38:56 +0300

    multiple project build config

--- flow_led/flow_led.c
+++ flow_led/flow_led.c
@@ -2,6 +2,7 @@
 #include "pico/cyw43_arch.h"
 
 int ledPins[] = {16, 17, 18, 19, 20, 21, 22, 26, 27, 28};
+
 int length = sizeof(ledPins) / sizeof(ledPins[0]);
 
 int main()
@@ -16,13 +17,13 @@ int main()
         for (int i = 0; i < length; i++)
         {
             gpio_put(ledPins[i], 1);
-            sleep_ms(100);
+            sleep_ms(500);
             gpio_put(ledPins[i], 0);
         }
         for (int i = length - 1; i > -1; i--)
         {
             gpio_put(ledPins[i], 1);
-            sleep_ms(100);
+            sleep_ms(500);
             gpio_put(ledPins[i], 0);
         }
     }
